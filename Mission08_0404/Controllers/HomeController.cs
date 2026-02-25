69b821dc36db7cd286ac2558071ab2c981dad1dc
githubCalvin <167842596+githubCalvin@users.noreply.github.com>
2026-02-25 16:55:27 -0700

    I just updated the view a little bit to match controller routing and to fix functionality

--- Mission08_0404/Controllers/HomeController.cs
+++ Mission08_0404/Controllers/HomeController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Mission08_0404.Models;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Mission08_0404.Controllers
 {
@@ -15,7 +16,12 @@ namespace Mission08_0404.Controllers
         }
         public IActionResult Index()
         {
-            return View();
+            var tasks = _context.Tasks
+                .Include(t => t.Cat)
+                .Include(t => t.Quadrant)
+                .ToList();
+
+            return View(tasks);
         }
         public IActionResult CompletedTasks()
         {
@@ -29,14 +35,33 @@ namespace Mission08_0404.Controllers
         }
         public IActionResult AddEditTasks(int? id)
         {
+            ViewBag.Categories = new SelectList(_context.Categories, "CatId", "CatName");
+
             if (id == null)
             {
                 return View(new TaskItem());
             }
 
             var task = _context.Tasks.Find(id);
-
             return View(task);
+        }
+        
+        [HttpPost]
+        public IActionResult AddEditTasks(TaskItem task)
+        {
+            ViewBag.Categories = new SelectList(_context.Categories, "CatId", "CatName");
+
+            if (task.TaskId == 0)
+            {
+                _context.Tasks.Add(task);
+            }
+            else
+            {
+                _context.Tasks.Update(task);
+            }
+
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
